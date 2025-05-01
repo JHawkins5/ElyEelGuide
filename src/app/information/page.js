a48941f9f7b1c6d3c108ffafb664851390e4e87f
Jack Hawkins <jhawkins__5@outlook.com>
2025-05-01 11:06:01 +0100

    Created information page

---
+++ src/app/information/page.js
@@ -0,0 +1,14 @@
+'use client';
+import React, { useState } from 'react';
+import './information.css';
+import Link from 'next/link';
+
+const Information = () => {
+    return (
+        <div>
+            <h1 className="title">Extra Tourist Information</h1>
+        </div>
+    );
+}
+
+export default Information;
