6120b86e1b001cd256b622dd819e3f8aabdf7a2a
Jack Hawkins <jhawkins__5@outlook.com>
2025-05-01 11:05:29 +0100

    Created activities page

---
+++ src/app/activities/page.js
@@ -0,0 +1,14 @@
+'use client';
+import React, { useState } from 'react';
+import './activities.css';
+import Link from 'next/link';
+
+const Activities = () => {
+    return (
+        <div>
+            <h1 className="title">Things to do in Ely</h1>
+        </div>
+    );
+}
+
+export default Activities;
