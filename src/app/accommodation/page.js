0ca6a10ec54a64cac8b2c66733666c0c81e399b3
Jack Hawkins <jhawkins__5@outlook.com>
2025-05-01 11:05:19 +0100

    Created accommodation page

---
+++ src/app/accommodation/page.js
@@ -0,0 +1,14 @@
+'use client';
+import React, { useState } from 'react';
+import './accommodation.css';
+import Link from 'next/link';
+
+const Accommodation = () => {
+    return (
+        <div>
+            <h1 className="title">Where to stay in Ely</h1>
+        </div>
+    );
+}
+
+export default Accommodation;
