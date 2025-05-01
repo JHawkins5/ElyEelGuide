8d8d8367ceaffb8a7cb1bd806d0cabf167465336
Jack Hawkins <jhawkins__5@outlook.com>
2025-05-01 11:04:02 +0100

    Added title

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,12 @@
 'use client';
-import React from 'react';
+import React, {useEffect} from 'react';
 import Link from "next/link";
 import './index.css';
 
 export default function Index() {
     return (
         <div>
-
+            <h1 className="greeting">Welcome to Ely!</h1>
         </div>
     );
 }
