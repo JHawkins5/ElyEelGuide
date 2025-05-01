b2ebbac23491544309b04eeab5471fd93166af2d
Jack Hawkins <jhawkins__5@outlook.com>
2025-05-01 11:05:52 +0100

    Created dining page

---
+++ src/app/dining/page.js
@@ -0,0 +1,14 @@
+'use client';
+import React, { useState } from 'react';
+import './dining.css';
+import Link from 'next/link';
+
+const Dining = () => {
+    return (
+        <div>
+            <h1 className="title">Cafés and Restaurants in Ely</h1>
+        </div>
+    );
+}
+
+export default Dining;
