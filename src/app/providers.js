9b26c7443511615b52dc7e8cf11c496637f213c6
Jack Hawkins <jhawkins__5@outlook.com>
2025-05-01 11:04:15 +0100

    Fixed

--- src/app/providers.js
+++ src/app/providers.js
@@ -3,5 +3,10 @@
 import Header from "../components/Header/Header";
 
 export function Providers({children}) {
-    return <Header />;
+    return (
+        <>
+            <Header />
+            {children}
+        </>
+    );
 }
