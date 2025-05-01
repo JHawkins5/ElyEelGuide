8c777dc1482fa532a3ca1700cb27ca6442bc246f
Jack Hawkins <jhawkins__5@outlook.com>
2025-05-01 11:06:13 +0100

    Created transport page

---
+++ src/app/transport/page.js
@@ -0,0 +1,14 @@
+'use client';
+import React, { useState } from 'react';
+import './transport.css';
+import Link from 'next/link';
+
+const Transport = () => {
+    return (
+        <div>
+            <h1 className="title">Transport to Ely</h1>
+        </div>
+    );
+}
+
+export default Transport;
