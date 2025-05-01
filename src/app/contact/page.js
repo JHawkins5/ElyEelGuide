c0542c0b34ace7cd7ea02e5cf0bcad15cca69a25
Jack Hawkins <jhawkins__5@outlook.com>
2025-05-01 11:05:40 +0100

    Created contact page

---
+++ src/app/contact/page.js
@@ -0,0 +1,14 @@
+'use client';
+import React, { useState } from 'react';
+import './contact.css';
+import Link from 'next/link';
+
+const Contact = () => {
+    return (
+        <div>
+            <h1 className="title">Contact us!</h1>
+        </div>
+    );
+}
+
+export default Contact;
