16ab95e213c95c7ab394dbfb4bac906173ba4cf3
Jack Hawkins <jhawkins__5@outlook.com>
2025-05-05 15:58:16 +0100

    Added contact info

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,12 +1,20 @@
 'use client';
 import React, { useState } from 'react';
-import './contact.css';
-import Link from 'next/link';
+import './contact.css'
 
 const Contact = () => {
     return (
         <div>
             <h1 className="title">Contact us!</h1>
+            <p>
+                If you have any questions or inquiries, you can contact us using either of the following:
+            </p>
+            <div className="contact-info">
+                <ul className="contact-list">
+                    <li>Email: <a href="mailto:help@elyeelguides.com">help@elyeelguides.com</a></li>
+                    <li>Telephone: 01353 999999</li>
+                </ul>
+            </div>
         </div>
     );
 }
