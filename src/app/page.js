c71d11ef057be2d4c2ab9a262fe62a68890d9e48
Jack Hawkins <jhawkins__5@outlook.com>
2025-05-05 15:47:00 +0100

    Started index page

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,34 @@ import './index.css';
 export default function Index() {
     return (
         <div>
-            <h1 className="greeting">Welcome to Ely!</h1>
+            <div className="banner">
+                <img className="banner-img" src="/banner.png" alt="Photo of Ely Cathedral"/>
+                <div className="banner-text">Welcome to Ely!</div>
+            </div>
+            <div className="ely-info">
+                <h2 className="list-title">Key facts about Ely</h2>
+                <ul className="ely-info-list">
+                    <li>Ely is a city in Cambridgeshire, England, located just 14 miles northeast of Cambridge, and 80
+                        miles north of London.
+                    </li>
+                    <li>The city was home to Oliver Cromwell from 1636 to 1646, prior to his rule as Lord Protector,
+                        when the country had no monarch.
+                    </li>
+                    <li>The name 'Ely' is believed to have originated from the Northumbrian word 'ēlġē', meaning
+                        'district of eels'.
+                    </li>
+                </ul>
+            </div>
+            <div className="site-info">
+                <p>
+                    Ely Eel Guides is an independent tourist site designed to celebrate Ely's heritage and highlight
+                    some of the city's hidden gems.
+                    Discover the history of Ely and its people, and learn about the city's unique culture and
+                    traditions, including eel fishing.
+                    A special feature of Ely Eel Guides is the eel recommender, which helps you find the best places to
+                    eat eel in Ely.
+                </p>
+            </div>
         </div>
     );
 }
