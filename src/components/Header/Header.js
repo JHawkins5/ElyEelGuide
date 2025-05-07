a4929a6e4a945a77d87e987f07ef16d81108722b
Jack Hawkins <jhawkins__5@outlook.com>
2025-05-07 19:19:57 +0100

    Changed close button icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -62,7 +62,7 @@ const Header = () => {
 
             {/* Sidebar - opens when the hamburger icon is clicked */}
             <nav className={`sidebar ${isSideBarOpen ? 'open' : ''}`}>
-                <button className="sidebar-close" onClick={toggleSideBar}>x</button>
+                <button className="sidebar-close" onClick={toggleSideBar}>&times;</button>
                 <ul>
                     <li><Link href="/">Home</Link></li>
                     <li><Link href="/transport">Transport</Link></li>
