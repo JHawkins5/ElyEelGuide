52148fd97c54b6db4459b1f0899696a55fff58c8
Jack Hawkins <jhawkins__5@outlook.com>
2025-05-07 11:19:10 +0100

    Created page

---
+++ src/app/dining/eel/page.js
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react';
+import './eel.css';
+import Link from 'next/link';
+
+const Eel = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Eel;
---
+++ src/app/dining/restaurants/page.js
@@ -0,0 +1,14 @@
+'use client';
+import React from 'react';
+import './restaurants.css';
+import Link from 'next/link';
+
+const Restaurants = () => {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Restaurants;
