9014279e6e2f65d6468e87f5e9af4662da7333c2
Jack Hawkins <jhawkins__5@outlook.com>
2025-05-10 09:58:23 +0100

    Improved accessibility

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,8 +3,14 @@ import Link from 'next/link';
 import './Button.css';
 
 const Button = ({href, children}) => {
+    let text = children;
+
+    if (text === "Website") {
+        text = "Go to website";
+    }
+
     return (
-        <Link className="button" href={href}>{children}</Link>
+        <Link className="button" href={href} aria-label={text} title={text}>{text}</Link>
     );
 }
 
