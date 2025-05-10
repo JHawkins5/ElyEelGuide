405cca79fb0b51cc40dc57fb528239501280645f
Jack Hawkins <jhawkins__5@outlook.com>
2025-05-10 09:16:28 +0100

    Refactored to improve readability and maintainability

--- src/app/transport/page.js
+++ src/app/transport/page.js
@@ -1,63 +1,61 @@
 'use client';
-import React, { useState } from 'react';
+import React from 'react';
 import './transport.css';
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import Link from "next/link";
 import Button from "@/components/Button/Button";
 
 const Transport = () => {
+    const transportMethods = [
+        {
+            title: "Travelling by Car",
+            content: "Ely is located along the A10, which stretches from Cambridge to King's Lynn. It is a 30 - 40 minute drive from Cambridge, and a 45 minute drive from King's Lynn. Ely is also accessible via the A142, which connects to the A14. The A14 stretches from the East Coast to the West Midlands.",
+        },
+        {
+            title: "Travelling by Train",
+            content: "Ely has its own train station, which is located just on the outskirts of the city centre, running alongside the River Great Ouse. The station has routes to and from Cambridge, King's Lynn, and Norwich in East Anglia, as well as to Stansted Airport, London King's Cross and Liverpool Street, and Birmingham New Street. At Cambridge, you can connect to Brighton, and at Peterborough, you can connect to cities in the North of England, and Scotland. The station is operated by Greater Anglia, and you can check the train times and book tickets on their website. Alternatively, you can use sites such as Trainline.",
+            links: [
+                { text: "Greater Anglia", href: "https://www.greateranglia.co.uk/" },
+                { text: "Trainline", href: "https://www.thetrainline.com/" }
+            ]
+        },
+        {
+            title: "Travelling by Bus",
+            content: "Ely is served by a number of bus routes, connecting the city to Cambridge, Newmarket, and the surrounding villages. For buses to Cambridge, you can take the 9. For buses to Newmarket, you can take the 112. You can find a full list of bus routes on Bustimes.",
+            links: [
+                { text: "Bustimes", href: "https://bustimes.org/localities/ely-cambs" }
+            ]
+        },
+        {
+            title: "Travelling by Plane",
+            content: "The nearest airport to Ely is London Stansted Airport. It is a 50 minute drive from Ely, via either Cambridge or Newmarket. Alternatively, Stansted Airport is accessible via a direct train from Ely station. You can visit the Stansted Airport website for more information on flights and transport to and from the airport.",
+            links: [
+                { text: "Stansted Airport", href: "https://www.stanstedairport.com/" }
+            ]
+        }]
+
     return (
         <div className="container">
             {/* Transport methods content */}
             <div className="content">
                 <h1 className="title">Transport to Ely</h1>
                 <div className="methods">
-                    <div className="method">
-                        <h2>Travelling by Car</h2>
-                        <p>
-                            Ely is located along the A10, which stretches from Cambridge to King's Lynn. It is a 30 - 40
-                            minute drive from Cambridge, and a 45 minute drive from King's Lynn.
-                            Ely is also accessible via the A142, which connects to the A14. The A14 stretches from the
-                            East Coast to the West Midlands.
-                        </p>
-                    </div>
-                    <div className="method">
-                        <h2>Travelling by Train</h2>
-                        <p>
-                            Ely has its own train station, which is located just on the outskirts of the city centre, running alongside the River Great Ouse.
-                            The station has routes to and from Cambridge, King's Lynn, and Norwich in East Anglia, as well as to Stansted Airport, London King's Cross and Liverpool Street, and Birmingham New Street.
-                            At Cambridge, you can connect to Brighton, and at Peterborough, you can connect to cities in the North of England, and Scotland.
-                        </p>
-                        <p>
-                            The station is operated by Greater Anglia, and you can check the train times and book tickets on their website. Alternatively, you can use sites such as Trainline.
-                        </p>
-                        <div className="buttons">
-                            <Button href="https://www.greateranglia.co.uk/">Greater Anglia</Button>
-                            <Button href="https://www.thetrainline.com/">Trainline</Button>
+                    {transportMethods.map((method, index) => (
+                        <div className="method" key={index}>
+                            <h2>{method.title}</h2>
+                            <p>{method.content}</p>
+
+                            {/* Add links if they exist */}
+                            {method.links && (
+                                <div className="buttons">
+                                    {method.links.map((link, linkIndex) => (
+                                        <Button key={linkIndex} href={link.href}>
+                                            {link.text}
+                                        </Button>
+                                    ))}
+                                </div>
+                            )}
                         </div>
-                    </div>
-                    <div className="method">
-                        <h2>Travelling by Bus</h2>
-                        <p>
-                            Ely is served by a number of bus routes, connecting the city to Cambridge, Newmarket, and the surrounding villages.
-                            For buses to Cambridge, you can take the 9. For buses to Newmarket, you can take the 112.
-                            You can find a full list of bus routes on Bustimes.
-                        </p>
-                        <div className="buttons">
-                            <Button href="https://bustimes.org/localities/ely-cambs">Bustimes</Button>
-                        </div>
-                    </div>
-                    <div className="method">
-                        <h2>Travelling by Plane</h2>
-                        <p>
-                            The nearest airport to Ely is London Stansted Airport. It is a 50 minute drive from Ely, via either Cambridge or Newmarket.
-                            Alternatively, Stansted Airport is accessible via a direct train from Ely station.
-                            You can visit the Stansted Airport website for more information on flights and transport to and from the airport.
-                        </p>
-                        <div className="buttons">
-                            <Button href="https://www.stanstedairport.com/">Stansted Airport</Button>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
             {/* Map using the leaflet library */}
